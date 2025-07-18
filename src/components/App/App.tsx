672e09df06a28bed64707eefd62c0fd9e5285d51
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-07-18 17:28:19 +0100

    noteServices

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,24 @@
 // import { useState } from "react";
+
+import SearchBox from "../SearchBox/SearchBox";
 import css from "./App.module.css";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  useQuery({
+    queryKey: ["notes"],
+    queryFn: async () => {
+      const response = await fetch("/api/notes");
+      return response.json();
+    },
+    refetchOnWindowFocus: false,
+  });
   return (
     <>
       <div className={css.app}>
         <header className={css.toolbar}>
-          {/* Компонент SearchBox */}
+          <SearchBox />
           {/* Пагінація */}
           {/* Кнопка створення нотатки */}
         </header>
---
+++ src/services/noteService.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import type { Note } from "../types/note";
+
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+const NOTEHUB_TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+// notehub-public.goit.study/api/notes?search=example&tag=Todo&page=1&perPage=10&sortBy=created'
+
+interface FeatchNotesProps {
+  page: number;
+  results: Note[];
+  total_pages: number;
+}
+export const featchNotes = async (
+  search: string,
+  page: number = 1,
+  perPage: number = 12,
+  tag?: string,
+  sortBy: string = "created"
+): Promise<FeatchNotesProps> => {
+  const config = {
+    params: {
+      search,
+      page,
+      perPage,
+      ...(tag ? { tag } : {}),
+      sortBy,
+    },
+    headers: {
+      Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+    },
+  };
+  const response = await axios.get(`/notes`, config);
+  return response.data;
+};
